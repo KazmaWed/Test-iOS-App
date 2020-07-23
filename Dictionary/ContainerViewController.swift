ad6d0df0793f98a00e85fd9e42bf7513b96f3038
Kazma Wed <kazmawed.github@gmail.com>
2020-07-23 16:14:53 +0900

    minor changes

--- Dictionary/ContainerViewController.swift
+++ Dictionary/ContainerViewController.swift
@@ -32,20 +32,16 @@ class ContainerViewController: UIViewController {
             //ラベルのサイズを調整
             self.textInContainer.sizeToFit()
             //ラベルの高さを取得
-            let contentHeight = self.textInContainer.frame.size.height
+            let contentsHeight = self.textInContainer.frame.size.height
             //スクロールビューのコンテンツサイズを調整
-            self.view.frame.size.height = contentHeight
+            self.view.frame.size.height = contentsHeight
             
             //スクロールビュー(親ビュー内)のコンテンツ高さ調整
             let parentVC = self.parent as! ModalViewController
-            parentVC.scrollView.contentSize.height = contentHeight
+            parentVC.scrollView.contentSize.height = contentsHeight
             
         }
         
     }
-    
-//    @objc func sizeFitToContents(_ notification: NSNotification) {
-//        sizeFitToContents()
-//    }
 
 }
